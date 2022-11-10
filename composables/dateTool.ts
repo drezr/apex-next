192c019c4730739d65924185eb9da0a401629868
drezr <ultimove@gmail.com>
2022-11-11 00:34:24 +0100

    rename filename

--- composables/dateTool.ts
+++ composables/dateTool.ts
@@ -1,5 +1,11 @@
 class DateTools {
   formatDatetimeNoYear(datetime: DateString) {
+    /**
+     * Returns datetime with the following format: DD-MM
+     *
+     * @param {DateString} datetime the date to convert. Expected format 'YYYY-MM-DD'
+     */
+
     let [year, month, day] = datetime.split('-')
 
     if (day.length == 1) day = '0' + day
